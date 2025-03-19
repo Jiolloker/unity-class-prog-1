df2d2d0e95cfce8e40d705e23e3967f3394d5771
jonathan <jiolloker@hotmail.com>
2025-03-19 20:50:40 -0300

    updation movement module

--- Player.cs
+++ Player.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //[SerializeField] private access but editable in inspector, this variable is for speed
-    [SerializeField] private float _moveSpeed= 3.5f;
-    
+    [SerializeField] private float _moveSpeed = 3.5f;
+
     //Axis
-    private float _xAxis= 0.0f, _yAxis = 0.0f;
+    private float _xAxis = 0.0f, _yAxis = 0.0f;
+
     //Movement
-    private Vector2 _dir = Vector2.zero;
+    private Vector3 _dir = Vector3.zero;
+    // private Vector2 _dir = Vector2.zero;
 
 
     // Update is called once per frame
@@ -41,21 +43,20 @@ public class Player : MonoBehaviour
         {
             _xAxis = 0.0f;
         }
+        
+        if (_xAxis != 0.0f || _yAxis != 0.0f)
+        {
+            Movement(_xAxis, _yAxis);
+        }
+        // Debug.Log($"Inputs: _xAxis: {_xAxis} _yAxis: {_yAxis}");
+    }
 
-        Debug.Log($"Inputs: _xAxis: {_xAxis} _yAxis: {_yAxis}");
+    private void Movement (float x, float y)
+    {
+        //limits the values to be between -1 and 1
+        _dir = (transform.right * x + transform.up * y).normalized;
+
+        //Time.deltaTime is the time between frames
+        transform.position += _dir * _moveSpeed * Time.deltaTime;
     }
 }
-
-
-
-    // Start is called before the first frame update
-    // private void Awake()
-    // {
-
-    // }
-
-    // // Update is called once per frame
-    // private void Start()
-    // {
-
-    // }
