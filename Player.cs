8d051070a8b9694a4c596a3cefbfa8e272057a58
jonathan <jiolloker@hotmail.com>
2025-03-19 20:53:55 -0300

    debugged movement

--- Player.cs
+++ Player.cs
@@ -16,47 +16,45 @@ public class Player : MonoBehaviour
 
 
     // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            _yAxis = 1.0f;
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            _yAxis = -1.0f;
-        }
-        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-        {
-            _yAxis = 0.0f;
-        }
+private void Update()
+{
+    // Reset axes to 0 at the start of each frame
+    _xAxis = 0.0f;
+    _yAxis = 0.0f;
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _xAxis = 1.0f;
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            _xAxis = -1.0f;
-        }
-        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
-        {
-            _xAxis = 0.0f;
-        }
-        
-        if (_xAxis != 0.0f || _yAxis != 0.0f)
-        {
-            Movement(_xAxis, _yAxis);
-        }
-        // Debug.Log($"Inputs: _xAxis: {_xAxis} _yAxis: {_yAxis}");
+    // Handle vertical movement (W and S keys)
+    if (Input.GetKey(KeyCode.W))
+    {
+        _yAxis = 1.0f;
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+        _yAxis = -1.0f;
     }
 
-    private void Movement (float x, float y)
+    // Handle horizontal movement (A and D keys)
+    if (Input.GetKey(KeyCode.D))
     {
-        //limits the values to be between -1 and 1
-        _dir = (transform.right * x + transform.up * y).normalized;
-
-        //Time.deltaTime is the time between frames
-        transform.position += _dir * _moveSpeed * Time.deltaTime;
+        _xAxis = 1.0f;
     }
+    else if (Input.GetKey(KeyCode.A))
+    {
+        _xAxis = -1.0f;
+    }
+
+    // If there is any input, call the Movement method
+    if (_xAxis != 0.0f || _yAxis != 0.0f)
+    {
+        Movement(_xAxis, _yAxis);
+    }
+}
+
+private void Movement(float x, float y)
+{
+    // Calculate direction and normalize it
+    _dir = (transform.right * x + transform.up * y).normalized;
+
+    // Move the object
+    transform.position += _dir * _moveSpeed * Time.deltaTime;
+}
 }
